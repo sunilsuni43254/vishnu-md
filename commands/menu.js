5db785612058e565cef7b7d652c277fdcc788d3f
blackmama12 <arunkumar773681@gmail.com>
2026-01-20 20:46:44 +0530

    Update menu.js

--- commands/menu.js
+++ commands/menu.js
@@ -62,7 +62,7 @@ export default async (sock, msg, args) => {
 
         await sock.sendMessage(chat, { react: { text: "✅", key: msg.key } });
 
-        const imagePath = './media/thumb.jpg'; 
+        const imagePath = './media/asura.jpg'; 
         const songPath = './media/song.opus'; 
 
         const menuText = `*👺⃝⃘̉̉̉━━━━━━━━━◆◆◆◆◆*
@@ -108,7 +108,7 @@ export default async (sock, msg, args) => {
         // 3. Send Image with Menu Text
 if (fs.existsSync(imagePath)) {
     await sock.sendMessage(chat, {
-        document: { url: './media/thumb.jpg' },
+        document: { url: './media/asura.jpg' },
         mimetype: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
         fileName: '👺 ASURA MD', 
         fileLength: 999,
