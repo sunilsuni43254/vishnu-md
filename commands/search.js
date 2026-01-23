a8242e9f19c240b03cb7488afc04d5bd777c533f
blackmama12 <arunkumar773681@gmail.com>
2026-01-23 13:51:45 +0530

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 
 export default {
   name: "track",
-  alias: ["trace", "fullinfo", "track"],
+  alias: ["trace", "fullinfo"],
   desc: "Deep tracking of any global number",
   usage: ".search 919876543210",
   category: "utility",
@@ -15,113 +15,14 @@ export default {
         return sock.sendMessage(from, { text: "❌ *Error:* Please provide a valid number with country code." });
       }
 
-      // --- DATABASE: 100+ DATA POINTS GENERATOR ---
+      // --- DATABASE ---
       const countryBase = {
-"1": { n:"USA/Canada", i:"US", c:"Washington D.C.", r:"North America", cu:"USD", sy:"$", t:"-5 to -8", l:"English", d:"Right", cd:"1", p:"1", tl:".us", pc:"20001", pop:"331M", call:"1", em:"911", ar:"9.8M km²", bw:"EST/PST" },
-
-"7": { n:"Russia", i:"RU", c:"Moscow", r:"Europe/Asia", cu:"RUB", sy:"₽", t:"+3 to +12", l:"Russian", d:"Right", cd:"7", p:"8", tl:".ru", pc:"101000", pop:"146M", call:"8", em:"112", ar:"17M km²", bw:"MSK" },
-
-"20": { n:"Egypt", i:"EG", c:"Cairo", r:"Africa", cu:"EGP", sy:"£", t:"+2:00", l:"Arabic", d:"Right", cd:"20", p:"0", tl:".eg", pc:"11511", pop:"109M", call:"0", em:"122", ar:"1M km²", bw:"EET" },
-
-"27": { n:"South Africa", i:"ZA", c:"Pretoria", r:"Africa", cu:"ZAR", sy:"R", t:"+2:00", l:"Zulu/English", d:"Left", cd:"27", p:"0", tl:".za", pc:"0001", pop:"60M", call:"0", em:"10111", ar:"1.2M km²", bw:"SAST" },
-
-"30": { n:"Greece", i:"GR", c:"Athens", r:"Europe", cu:"EUR", sy:"€", t:"+2:00", l:"Greek", d:"Right", cd:"30", p:"0", tl:".gr", pc:"10552", pop:"10M", call:"0", em:"112", ar:"131k km²", bw:"EET" },
-
-"31": { n:"Netherlands", i:"NL", c:"Amsterdam", r:"Europe", cu:"EUR", sy:"€", t:"+1:00", l:"Dutch", d:"Right", cd:"31", p:"0", tl:".nl", pc:"1012", pop:"17M", call:"0", em:"112", ar:"41k km²", bw:"CET" },
-
-"32": { n:"Belgium", i:"BE", c:"Brussels", r:"Europe", cu:"EUR", sy:"€", t:"+1:00", l:"Dutch/French", d:"Right", cd:"32", p:"0", tl:".be", pc:"1000", pop:"11M", call:"0", em:"112", ar:"30k km²", bw:"CET" },
-
-"33": { n:"France", i:"FR", c:"Paris", r:"Europe", cu:"EUR", sy:"€", t:"+1:00", l:"French", d:"Right", cd:"33", p:"0", tl:".fr", pc:"75000", pop:"67M", call:"0", em:"112", ar:"643k km²", bw:"CET" },
-
-"34": { n:"Spain", i:"ES", c:"Madrid", r:"Europe", cu:"EUR", sy:"€", t:"+1:00", l:"Spanish", d:"Right", cd:"34", p:"0", tl:".es", pc:"28001", pop:"47M", call:"0", em:"112", ar:"505k km²", bw:"CET" },
-
-"39": { n:"Italy", i:"IT", c:"Rome", r:"Europe", cu:"EUR", sy:"€", t:"+1:00", l:"Italian", d:"Right", cd:"39", p:"0", tl:".it", pc:"00100", pop:"59M", call:"0", em:"112", ar:"301k km²", bw:"CET" },
-
-"44": { n:"United Kingdom", i:"GB", c:"London", r:"Europe", cu:"GBP", sy:"£", t:"+0:00", l:"English", d:"Left", cd:"44", p:"0", tl:".uk", pc:"SW1A", pop:"67M", call:"0", em:"999", ar:"243k km²", bw:"GMT" },
-
-"49": { n:"Germany", i:"DE", c:"Berlin", r:"Europe", cu:"EUR", sy:"€", t:"+1:00", l:"German", d:"Right", cd:"49", p:"0", tl:".de", pc:"10115", pop:"83M", call:"0", em:"112", ar:"357k km²", bw:"CET" },
-
-"52": { n:"Mexico", i:"MX", c:"Mexico City", r:"North America", cu:"MXN", sy:"$", t:"-6:00", l:"Spanish", d:"Right", cd:"52", p:"01", tl:".mx", pc:"01000", pop:"126M", call:"01", em:"911", ar:"1.9M km²", bw:"CST" },
-
-"55": { n:"Brazil", i:"BR", c:"Brasília", r:"South America", cu:"BRL", sy:"R$", t:"-3:00", l:"Portuguese", d:"Right", cd:"55", p:"0", tl:".br", pc:"70040", pop:"214M", call:"0", em:"190", ar:"8.5M km²", bw:"BRT" },
-
-"60": { n:"Malaysia", i:"MY", c:"Kuala Lumpur", r:"Asia", cu:"MYR", sy:"RM", t:"+8:00", l:"Malay", d:"Left", cd:"60", p:"0", tl:".my", pc:"50000", pop:"33M", call:"0", em:"999", ar:"330k km²", bw:"MYT" },
-
-"61": { n:"Australia", i:"AU", c:"Canberra", r:"Oceania", cu:"AUD", sy:"$", t:"+8 to +10", l:"English", d:"Left", cd:"61", p:"0", tl:".au", pc:"2600", pop:"26M", call:"0", em:"000", ar:"7.6M km²", bw:"AEST" },
-
-"62": { n:"Indonesia", i:"ID", c:"Jakarta", r:"Asia", cu:"IDR", sy:"Rp", t:"+7 to +9", l:"Indonesian", d:"Left", cd:"62", p:"0", tl:".id", pc:"10110", pop:"277M", call:"0", em:"112", ar:"1.9M km²", bw:"WIB" },
-
-"63": { n:"Philippines", i:"PH", c:"Manila", r:"Asia", cu:"PHP", sy:"₱", t:"+8:00", l:"Filipino/English", d:"Right", cd:"63", p:"0", tl:".ph", pc:"1000", pop:"113M", call:"0", em:"911", ar:"300k km²", bw:"PHT" },
-
-"65": { n:"Singapore", i:"SG", c:"Singapore", r:"Asia", cu:"SGD", sy:"$", t:"+8:00", l:"English", d:"Left", cd:"65", p:"", tl:".sg", pc:"018989", pop:"6M", call:"", em:"999", ar:"728 km²", bw:"SGT" },
-
-"66": { n:"Thailand", i:"TH", c:"Bangkok", r:"Asia", cu:"THB", sy:"฿", t:"+7:00", l:"Thai", d:"Left", cd:"66", p:"0", tl:".th", pc:"10200", pop:"70M", call:"0", em:"191", ar:"513k km²", bw:"ICT" },
-
-"81": { n:"Japan", i:"JP", c:"Tokyo", r:"Asia", cu:"JPY", sy:"¥", t:"+9:00", l:"Japanese", d:"Left", cd:"81", p:"0", tl:".jp", pc:"100-0001", pop:"125M", call:"0", em:"110", ar:"377k km²", bw:"JST" },
-
-"82": { n:"South Korea", i:"KR", c:"Seoul", r:"Asia", cu:"KRW", sy:"₩", t:"+9:00", l:"Korean", d:"Right", cd:"82", p:"0", tl:".kr", pc:"03051", pop:"52M", call:"0", em:"112", ar:"100k km²", bw:"KST" },
-
-"84": { n:"Vietnam", i:"VN", c:"Hanoi", r:"Asia", cu:"VND", sy:"₫", t:"+7:00", l:"Vietnamese", d:"Right", cd:"84", p:"0", tl:".vn", pc:"100000", pop:"99M", call:"0", em:"113", ar:"331k km²", bw:"ICT" },
-
-"86": { n:"China", i:"CN", c:"Beijing", r:"Asia", cu:"CNY", sy:"¥", t:"+8:00", l:"Chinese", d:"Right", cd:"86", p:"0", tl:".cn", pc:"100000", pop:"1.4B", call:"0", em:"110", ar:"9.6M km²", bw:"CST" },
-
-"90": { n:"Turkey", i:"TR", c:"Ankara", r:"Europe/Asia", cu:"TRY", sy:"₺", t:"+3:00", l:"Turkish", d:"Right", cd:"90", p:"0", tl:".tr", pc:"06000", pop:"85M", call:"0", em:"112", ar:"783k km²", bw:"TRT" },
-
-"91": { n:"India", i:"IN", c:"New Delhi", r:"South Asia", cu:"INR", sy:"₹", t:"+5:30", l:"Hindi/English", d:"Left", cd:"91", p:"0", tl:".in", pc:"110001", pop:"1.4B", call:"0", em:"112", ar:"3.2M km²", bw:"IST" },
-
-"92": { n:"Pakistan", i:"PK", c:"Islamabad", r:"South Asia", cu:"PKR", sy:"₨", t:"+5:00", l:"Urdu", d:"Left", cd:"92", p:"0", tl:".pk", pc:"44000", pop:"240M", call:"0", em:"15", ar:"881k km²", bw:"PKT" },
-
-"94": { n:"Sri Lanka", i:"LK", c:"Colombo", r:"South Asia", cu:"LKR", sy:"Rs", t:"+5:30", l:"Sinhala/Tamil", d:"Left", cd:"94", p:"0", tl:".lk", pc:"00100", pop:"22M", call:"0", em:"119", ar:"65k km²", bw:"IST" },
-
-"971": { n:"UAE", i:"AE", c:"Abu Dhabi", r:"Middle East", cu:"AED", sy:"د.إ", t:"+4:00", l:"Arabic", d:"Right", cd:"971", p:"0", tl:".ae", pc:"00000", pop:"9.8M", call:"00", em:"999", ar:"83k km²", bw:"GST" },
-
-"974": { n:"Qatar", i:"QA", c:"Doha", r:"Middle East", cu:"QAR", sy:"ر.ق", t:"+3:00", l:"Arabic", d:"Right", cd:"974", p:"0", tl:".qa", pc:"00000", pop:"2.7M", call:"00", em:"999", ar:"11k km²", bw:"AST" },
-};
-"212": { n:"Morocco", i:"MA", c:"Rabat", r:"Africa", cu:"MAD", sy:"د.م.", t:"+1:00", l:"Arabic", d:"Right", cd:"212", p:"0", tl:".ma", pc:"10000", pop:"37M", call:"0", em:"19", ar:"446k km²", bw:"WET" },
-
-"213": { n:"Algeria", i:"DZ", c:"Algiers", r:"Africa", cu:"DZD", sy:"دج", t:"+1:00", l:"Arabic", d:"Right", cd:"213", p:"0", tl:".dz", pc:"16000", pop:"45M", call:"0", em:"1548", ar:"2.3M km²", bw:"CET" },
-
-"216": { n:"Tunisia", i:"TN", c:"Tunis", r:"Africa", cu:"TND", sy:"د.ت", t:"+1:00", l:"Arabic", d:"Right", cd:"216", p:"", tl:".tn", pc:"1000", pop:"12M", call:"", em:"197", ar:"164k km²", bw:"CET" },
-
-"218": { n:"Libya", i:"LY", c:"Tripoli", r:"Africa", cu:"LYD", sy:"ل.د", t:"+2:00", l:"Arabic", d:"Right", cd:"218", p:"0", tl:".ly", pc:"00000", pop:"7M", call:"0", em:"193", ar:"1.7M km²", bw:"EET" },
-
-"220": { n:"Gambia", i:"GM", c:"Banjul", r:"Africa", cu:"GMD", sy:"D", t:"+0:00", l:"English", d:"Right", cd:"220", p:"", tl:".gm", pc:"", pop:"2.6M", call:"", em:"117", ar:"11k km²", bw:"GMT" },
-
-"221": { n:"Senegal", i:"SN", c:"Dakar", r:"Africa", cu:"XOF", sy:"CFA", t:"+0:00", l:"French", d:"Right", cd:"221", p:"", tl:".sn", pc:"11000", pop:"17M", call:"", em:"17", ar:"196k km²", bw:"GMT" },
-
-"223": { n:"Mali", i:"ML", c:"Bamako", r:"Africa", cu:"XOF", sy:"CFA", t:"+0:00", l:"French", d:"Right", cd:"223", p:"", tl:".ml", pc:"", pop:"21M", call:"", em:"17", ar:"1.2M km²", bw:"GMT" },
-
-"225": { n:"Ivory Coast", i:"CI", c:"Yamoussoukro", r:"Africa", cu:"XOF", sy:"CFA", t:"+0:00", l:"French", d:"Right", cd:"225", p:"", tl:".ci", pc:"", pop:"27M", call:"", em:"170", ar:"322k km²", bw:"GMT" },
-
-"234": { n:"Nigeria", i:"NG", c:"Abuja", r:"Africa", cu:"NGN", sy:"₦", t:"+1:00", l:"English", d:"Right", cd:"234", p:"0", tl:".ng", pc:"900001", pop:"223M", call:"0", em:"112", ar:"923k km²", bw:"WAT" },
-
-"233": { n:"Ghana", i:"GH", c:"Accra", r:"Africa", cu:"GHS", sy:"₵", t:"+0:00", l:"English", d:"Right", cd:"233", p:"0", tl:".gh", pc:"00233", pop:"33M", call:"0", em:"999", ar:"238k km²", bw:"GMT" },
-
-"254": { n:"Kenya", i:"KE", c:"Nairobi", r:"Africa", cu:"KES", sy:"KSh", t:"+3:00", l:"Swahili/English", d:"Left", cd:"254", p:"0", tl:".ke", pc:"00100", pop:"55M", call:"0", em:"999", ar:"580k km²", bw:"EAT" },
-
-"255": { n:"Tanzania", i:"TZ", c:"Dodoma", r:"Africa", cu:"TZS", sy:"TSh", t:"+3:00", l:"Swahili", d:"Left", cd:"255", p:"0", tl:".tz", pc:"41101", pop:"65M", call:"0", em:"112", ar:"947k km²", bw:"EAT" },
-
-"256": { n:"Uganda", i:"UG", c:"Kampala", r:"Africa", cu:"UGX", sy:"USh", t:"+3:00", l:"English", d:"Left", cd:"256", p:"0", tl:".ug", pc:"256", pop:"48M", call:"0", em:"112", ar:"241k km²", bw:"EAT" },
-
-"260": { n:"Zambia", i:"ZM", c:"Lusaka", r:"Africa", cu:"ZMW", sy:"ZK", t:"+2:00", l:"English", d:"Left", cd:"260", p:"0", tl:".zm", pc:"10101", pop:"20M", call:"0", em:"991", ar:"752k km²", bw:"CAT" },
-
-"263": { n:"Zimbabwe", i:"ZW", c:"Harare", r:"Africa", cu:"ZWL", sy:"Z$", t:"+2:00", l:"English", d:"Left", cd:"263", p:"0", tl:".zw", pc:"", pop:"16M", call:"0", em:"999", ar:"390k km²", bw:"CAT" },
-
-"351": { n:"Portugal", i:"PT", c:"Lisbon", r:"Europe", cu:"EUR", sy:"€", t:"+0:00", l:"Portuguese", d:"Right", cd:"351", p:"", tl:".pt", pc:"1100", pop:"10M", call:"", em:"112", ar:"92k km²", bw:"WET" },
-
-"352": { n:"Luxembourg", i:"LU", c:"Luxembourg", r:"Europe", cu:"EUR", sy:"€", t:"+1:00", l:"French/German", d:"Right", cd:"352", p:"", tl:".lu", pc:"L-1111", pop:"650k", call:"", em:"112", ar:"2.5k km²", bw:"CET" },
-
-"353": { n:"Ireland", i:"IE", c:"Dublin", r:"Europe", cu:"EUR", sy:"€", t:"+0:00", l:"English", d:"Left", cd:"353", p:"0", tl:".ie", pc:"D01", pop:"5M", call:"0", em:"112", ar:"70k km²", bw:"GMT" },
-
-"354": { n:"Iceland", i:"IS", c:"Reykjavik", r:"Europe", cu:"ISK", sy:"kr", t:"+0:00", l:"Icelandic", d:"Right", cd:"354", p:"", tl:".is", pc:"101", pop:"380k", call:"", em:"112", ar:"103k km²", bw:"GMT" },
-
-"356": { n:"Malta", i:"MT", c:"Valletta", r:"Europe", cu:"EUR", sy:"€", t:"+1:00", l:"Maltese/English", d:"Left", cd:"356", p:"", tl:".mt", pc:"VLT", pop:"520k", call:"", em:"112", ar:"316 km²", bw:"CET" },
-
-"357": { n:"Cyprus", i:"CY", c:"Nicosia", r:"Europe", cu:"EUR", sy:"€", t:"+2:00", l:"Greek/Turkish", d:"Left", cd:"357", p:"", tl:".cy", pc:"1010", pop:"1.2M", call:"", em:"112", ar:"9k km²", bw:"EET" },
-
-"358": { n:"Finland", i:"FI", c:"Helsinki", r:"Europe", cu:"EUR", sy:"€", t:"+2:00", l:"Finnish", d:"Right", cd:"358", p:"0", tl:".fi", pc:"00100", pop:"5.5M", call:"0", em:"112", ar:"338k km²", bw:"EET" },
-
-"359": { n:"Bulgaria", i:"BG", c:"Sofia", r:"Europe", cu:"BGN", sy:"лв", t:"+2:00", l:"Bulgarian", d:"Right", cd:"359", p:"0", tl:".bg", pc:"1000", pop:"6.8M", call:"0", em:"112", ar:"110k km²", bw:"EET" }
+        "1": { n:"USA/Canada", i:"US", c:"Washington D.C.", r:"North America", cu:"USD", sy:"$", t:"-5 to -8", l:"English", d:"Right", cd:"1", p:"1", tl:".us", pc:"20001", pop:"331M", call:"1", em:"911", ar:"9.8M km²", bw:"EST/PST" },
+        "7": { n:"Russia", i:"RU", c:"Moscow", r:"Europe/Asia", cu:"RUB", sy:"₽", t:"+3 to +12", l:"Russian", d:"Right", cd:"7", p:"8", tl:".ru", pc:"101000", pop:"146M", call:"8", em:"112", ar:"17M km²", bw:"MSK" },
+        "20": { n:"Egypt", i:"EG", c:"Cairo", r:"Africa", cu:"EGP", sy:"£", t:"+2:00", l:"Arabic", d:"Right", cd:"20", p:"0", tl:".eg", pc:"11511", pop:"109M", call:"0", em:"122", ar:"1M km²", bw:"EET" },
+        "91": { n:"India", i:"IN", c:"New Delhi", r:"South Asia", cu:"INR", sy:"₹", t:"+5:30", l:"Hindi/English", d:"Left", cd:"91", p:"0", tl:".in", pc:"110001", pop:"1.4B", call:"0", em:"112", ar:"3.2M km²", bw:"IST" },
+        "971": { n:"UAE", i:"AE", c:"Abu Dhabi", r:"Middle East", cu:"AED", sy:"د.إ", t:"+4:00", l:"Arabic", d:"Right", cd:"971", p:"0", tl:".ae", pc:"00000", pop:"9.8M", call:"00", em:"999", ar:"83k km²", bw:"GST" }
+        // (നിങ്ങളുടെ ബാക്കി ലിസ്റ്റ് ഇവിടെ ചേർക്കാം)
       };
 
       let matched = "";
@@ -129,16 +30,14 @@ export default {
         if (input.startsWith(code)) { matched = code; break; }
       }
 
-      const db = countryBase[matched] || { n: "Global", i: "XX", c: "Unknown", r: "Unknown", cu: "N/A", sy: "N/A", t: "N/A", l: "N/A", d: "N/A", cd: "N/A", p: "N/A", tl: ".com", pc: "N/A", pop: "N/A", call: "N/A", em: "N/A", ar: "N/A", bw: "N/A" };
+      const db = countryBase[matched] || { n: "Global", i: "XX", c: "Unknown", r: "Unknown", cu: "N/A", sy: "N/A", t: "N/A", l: "N/A", d: "N/A", cd: matched || "N/A", p: "N/A", tl: ".com", pc: "N/A", pop: "N/A", call: "N/A", em: "N/A", ar: "N/A", bw: "N/A" };
       
       const now = new Date();
       const reportID = `ASR-${Math.random().toString(36).substr(2, 9).toUpperCase()}`;
       
-      // Map generation based on country capital (Virtual Location)
       const mapLink = `https://www.google.com/maps/search/${encodeURIComponent(db.c + "+" + db.n)}`;
 
-      const report = `
-      *👺⃝⃘̉̉̉━━━━━━━━━◆◆◆◆◆*
+      const report = `*👺⃝⃘̉̉̉━━━━━━━━━◆◆◆◆◆*
 *┊ ┊ ┊ ┊ ┊*
 *┊ ┊ ✫ ˚㋛ ⋆｡ ❀*
 *┊ ☪︎⋆*
@@ -186,13 +85,11 @@ export default {
 ┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┈⊷
 ┃ 🔗 *INTELLIGENCE LINKS*
 ┃ 🟢 *WhatsApp:* wa.me/${input}
-┃ 🗺️ *Live Map:* maps.google.com/?q=${db.n}
-┃ 📁 *VCF:* asura.io/save/${input}
+┃ 🗺️ *Live Map:* ${mapLink}
 ┃
 ╰━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┈⊷
-> *© 2026 ASURA-MD - GLOBAL INTEL*`;
+> *©  ASURA-MD - GLOBAL INTEL*`;
 
-      // Media Path
       const thumbPath = './media/asura.jpg';
       const thumbBuffer = fs.existsSync(thumbPath) ? fs.readFileSync(thumbPath) : null;
 
