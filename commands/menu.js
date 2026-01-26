c012dbdd82f0eefbf85bef13333a10130ef09454
blackmama12 <arunkumar773681@gmail.com>
2026-01-26 07:34:44 +0530

    Update menu.js

--- commands/menu.js
+++ commands/menu.js
@@ -84,7 +84,7 @@ export default async (sock, msg, args) => {
 ┃ ⊙🖼️   .Sticker
 ┃ ⊙🎲   .Game
 ┃ ⊙🎭   .Fun
-┃ ⊙ℹ   .Information <number/Link>
+┃ ⊙🤖   .Ai
 ┃ ⊙🆎️   .Font <text>
 ┃ ⊙👤   .Owner
 ┃ ⊙🤯   .Play
