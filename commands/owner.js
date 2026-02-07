a59f55743b00137d5b517f16a035361c653ee95c
blackmama12 <arunkumar773681@gmail.com>
2026-02-07 07:14:24 +0530

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -29,7 +29,7 @@ export default async (sock, msg, args) => {
     // 2. V-Card Contact Details
     const vcard = 'BEGIN:VCARD\n' +
                 'VERSION:3.0\n' + 
-                'FN:Arun Cumar\n' + 
+                'FN:arun•°Cumar\n' + 
                 'ORG:Asura MD Developer;\n' + 
                 'TEL;type=CELL;type=VOICE;waid=917736811908:+91 7736811908\n' + 
                 'END:VCARD';
@@ -48,7 +48,7 @@ export default async (sock, msg, args) => {
         // Step 2: ഡെവലപ്പർ കോൺടാക്റ്റ് കാർഡ് അയക്കുന്നു
         await sock.sendMessage(chat, { 
             contacts: { 
-                displayName: 'arun Cumar', 
+                displayName: 'arun•°Cumar', 
                 contacts: [{ vcard }] 
             } 
         }, { quoted: msg });
@@ -59,7 +59,7 @@ export default async (sock, msg, args) => {
                 degreesLatitude: 10.8505, 
                 degreesLongitude: 76.2711 
             },
-            name: 'Arun Cumar - Asura MD HQ',
+            name: 'arun•°Cumar - Asura-MD HQ',
             address: 'Kerala, India 🇮🇳',
             comment: 'The home of Asura MD Development'
         }, { quoted: msg });
